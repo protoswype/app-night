b1189f8b5ff277a887d1d68c57ba6a66045003e6
Sebastian Binder <designstudio@sebastianbinder.com>
2014-11-08 19:50:46 +0100

    menu

---
+++ app/src/main/java/lmu/appnight/MainActivity.java
@@ -0,0 +1,161 @@
+package lmu.appnight;
+
+import com.google.android.glass.widget.CardBuilder;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
+import com.google.android.glass.media.Sounds;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * An {@link Activity} showing a tuggable "Hello World!" card.
+ * <p/>
+ * The main content view is composed of a one-card {@link CardScrollView} that provides tugging
+ * feedback to the user when swipe gestures are detected.
+ * If your Glassware intends to intercept swipe gestures, you should set the content view directly
+ * and use a {@link com.google.android.glass.touchpad.GestureDetector}.
+ *
+ * @see <a href="https://developers.google.com/glass/develop/gdk/touch">GDK Developer Guide</a>
+ */
+
+
+/**
+ * Creates a card scroll view with examples of different image layout cards.
+*/
+public final class MainActivity extends Activity {
+
+    private CardScrollAdapter mAdapter;
+    private CardScrollView mCardScroller;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        mAdapter = new CardAdapter(createMainMenu(this));
+        mCardScroller = new CardScrollView(this);
+        mCardScroller.setAdapter(mAdapter);
+        setContentView(mCardScroller);
+        setCardScrollerListener();
+    }
+
+
+    static final int PERSON_KO = 0;
+    static final int FIRST_AID = 1;
+    static final int EMERGENCY_EXIT = 2;
+    static final int HUNGRY = 3;
+
+
+    /**
+     * Creates list of cards that showcase different type of {@link CardBuilder} API.
+    */
+    private List<CardBuilder> createMainMenu(Context context) {
+        ArrayList<CardBuilder> cards = new ArrayList<CardBuilder>();
+
+        cards.add(PERSON_KO, new CardBuilder(context, CardBuilder.Layout.MENU)
+                .setText(R.string.person_ko_menu_text)
+                .setIcon(R.drawable.ic_person_ko_menu)
+                .setFootnote(R.string.person_ko_menu_desc));
+        cards.add(FIRST_AID, new CardBuilder(context, CardBuilder.Layout.MENU)
+                .setText(R.string.first_aid_kit_menu_text)
+                .setIcon(R.drawable.ic_first_aid_kit_menu)
+                .setFootnote(R.string.first_aid_kit_menu_desc));
+        cards.add(EMERGENCY_EXIT, new CardBuilder(context, CardBuilder.Layout.MENU)
+                .setText(R.string.emergency_exit_menu_text)
+                .setIcon(R.drawable.ic_emergency_exit_menu)
+                .setFootnote(R.string.emergency_exit_menu_desc));
+        cards.add(HUNGRY, new CardBuilder(context, CardBuilder.Layout.MENU)
+                .setText(R.string.hungry_menu_text)
+                .setIcon(R.drawable.ic_hungry_menu)
+                .setFootnote(R.string.hungry_menu_desc));
+
+        return cards;
+    }
+
+
+
+    private List<CardBuilder> createFirstAidMainLocationsMenu(Context context) {
+        ArrayList<CardBuilder> cards = new ArrayList<CardBuilder>();
+
+        cards.add(new CardBuilder(context, CardBuilder.Layout.COLUMNS)
+                .setText(R.string.first_aid_main_entrance_text)
+                .addImage(R.drawable.img_first_aid_main_entrance)
+                .setFootnote(R.string.first_aid_main_entrance_desc));
+
+        return cards;
+    }
+
+    private List<CardBuilder> createHungryLocationsMenu(Context context) {
+        ArrayList<CardBuilder> cards = new ArrayList<CardBuilder>();
+
+        cards.add(new CardBuilder(context, CardBuilder.Layout.COLUMNS)
+                .setText(R.string.hungry_text)
+                .addImage(R.drawable.img_hungry)
+                .setFootnote(R.string.hungry_desc));
+
+        return cards;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCardScroller.activate();
+    }
+
+    @Override
+    protected void onPause() {
+        mCardScroller.deactivate();
+        super.onPause();
+    }
+
+    /**
+     * Different type of activities can be shown, when tapped on a card.
+     */
+    private void setCardScrollerListener() {
+        mCardScroller.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Log.d(TAG, "Clicked view at position " + position + ", row-id " + id);
+                int soundEffect = Sounds.TAP;
+                switch (position) {
+                    case PERSON_KO:
+                        //mCardScroller.setAdapter(new CardAdapter(createPersonKoMenu(this)));
+                        //new CardAdapter(createPersonKoMenu(this));
+                        startActivity(new Intent(MainActivity.this, PersonKoActivity.class));
+                        break;
+                    case EMERGENCY_EXIT:
+                        //mCardScroller.setAdapter(new CardAdapter(createMainMenu(this)));
+                        //startActivity(new Intent(MainActivity.this, MainActivity.class));
+                        break;
+                    case FIRST_AID:
+                        //mCardScroller.setAdapter(new CardAdapter(createMainMenu(this)));
+                        //startActivity(new Intent(MainActivity.this, MainActivity.class));
+                        break;
+                    default:
+                        soundEffect = Sounds.ERROR;
+                        //Log.d(TAG, "Don't show anything");
+                }
+
+                // Play sound.
+                AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                am.playSoundEffect(soundEffect);
+            }
+        });
+    }
+
+}
