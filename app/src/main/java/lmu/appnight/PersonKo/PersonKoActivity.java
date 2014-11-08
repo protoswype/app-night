4f4b590ccc642068c6a84f3ffc74c562712b03ad
Michael Koriath <mail@codeline.me>
2014-11-08 23:27:39 +0100

    Revive

--- app/src/main/java/lmu/appnight/PersonKo/PersonKoActivity.java
+++ app/src/main/java/lmu/appnight/PersonKo/PersonKoActivity.java
@@ -1,5 +1,6 @@
 package lmu.appnight.PersonKo;
 
+import com.google.android.glass.media.Sounds;
 import com.google.android.glass.widget.CardBuilder;
 import com.google.android.glass.widget.CardScrollAdapter;
 import com.google.android.glass.widget.CardScrollView;
@@ -7,12 +8,20 @@ import com.google.android.glass.widget.CardScrollView;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import lmu.appnight.Classes.CardAdapter;
+import lmu.appnight.FirstAidKit.FirstAidKitActivity;
+import lmu.appnight.Hungry.HungryActivity;
+import lmu.appnight.PersonKoExplanation.PersonKoExplanationActivity;
 import lmu.appnight.R;
 
 /**
@@ -25,6 +34,8 @@ public final class PersonKoActivity extends Activity {
     private CardScrollView mCardScroller;
 
     static final int PERSON_KO_EXPLANATION = 0;
+    static final int CALL_AMBULANCE = 1;
+
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -34,6 +45,7 @@ public final class PersonKoActivity extends Activity {
         mCardScroller = new CardScrollView(this);
         mCardScroller.setAdapter(mAdapter);
         setContentView(mCardScroller);
+        setCardScrollerListener();
     }
 
 
@@ -44,7 +56,7 @@ public final class PersonKoActivity extends Activity {
         cards.add(PERSON_KO_EXPLANATION, new CardBuilder(context, CardBuilder.Layout.MENU)
                 .setText(R.string.recovery_position_menu_text)
                 .setIcon(R.drawable.ic_person_ko_menu));
-          cards.add(new CardBuilder(context, CardBuilder.Layout.MENU)
+        cards.add(CALL_AMBULANCE, new CardBuilder(context, CardBuilder.Layout.MENU)
                 .setText(R.string.emergency_call_menu_text)
                 .setIcon(R.drawable.ic_emergency_call_menu));
 
@@ -63,5 +75,31 @@ public final class PersonKoActivity extends Activity {
         mCardScroller.deactivate();
         super.onPause();
     }
+
+    private void setCardScrollerListener() {
+        mCardScroller.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Log.d(TAG, "Clicked view at position " + position + ", row-id " + id);
+                int soundEffect = Sounds.TAP;
+                switch (position) {
+                    case PERSON_KO_EXPLANATION:
+                        startActivity(new Intent(PersonKoActivity.this, PersonKoExplanationActivity.class));
+                        break;
+                    case CALL_AMBULANCE:
+                        break;
+                    default:
+                        soundEffect = Sounds.ERROR;
+                        //Log.d(TAG, "Don't show anything");
+                }
+
+                // Play sound.
+                AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                am.playSoundEffect(soundEffect);
+            }
+        });
+    }
+
 
 }
